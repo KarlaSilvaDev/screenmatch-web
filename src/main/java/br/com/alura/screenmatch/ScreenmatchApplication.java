28f62c01ae1149b2cb7a1984e4e190c08be09d12
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-06-02 13:58:38 -0300

    modified: class ScreenmatchApplication

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,10 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.SerieData;
-import br.com.alura.screenmatch.service.ApiConsumer;
-import br.com.alura.screenmatch.service.DataConverter;
+import br.com.alura.screenmatch.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Ctrl + Alt + O : tira todos os imports que não estão sendo usados na classe
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
@@ -16,12 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var apiConsumer = new ApiConsumer(); //inferência de tipo
-		var json = apiConsumer.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-
-		DataConverter converter = new DataConverter();
-		SerieData data = converter.getData(json, SerieData.class);
-		System.out.println(data);
+		Main main = new Main();
+		main.showMenu();
 	}
 }
