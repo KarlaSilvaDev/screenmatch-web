08617a5ba3f62d8fb35940b59b8451a7596d64d7
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-06-18 15:55:34 -0300

    modified: included genre, actors, posterUrl and plot attributes in record SerieData

--- src/main/java/br/com/alura/screenmatch/model/SerieData.java
+++ src/main/java/br/com/alura/screenmatch/model/SerieData.java
@@ -6,5 +6,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SerieData(@JsonAlias("Title") String title,
                         @JsonAlias("totalSeasons") Integer numberOfSeasons,
-                        @JsonAlias("imdbRating") String rating) {
-}
+                        @JsonAlias("imdbRating") String rating,
+                        @JsonAlias("Genre") String genre,
+                        @JsonAlias("Actors") String actors,
+                        @JsonAlias("Poster") String posterUrl,
+                        @JsonAlias("Plot") String plot
+                        ) {}
