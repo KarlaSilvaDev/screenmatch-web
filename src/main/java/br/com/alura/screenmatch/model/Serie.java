a7db4adc9ff05734f4b42236ceac21dff9414b12
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-06-29 18:53:29 -0300

    created setters, getters e overrided toString from class Serie

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -21,4 +21,73 @@ public class Serie {
         this.posterUrl = serieData.posterUrl();
         this.plot = serieData.plot();
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getNumberOfSeasons() {
+        return numberOfSeasons;
+    }
+
+    public void setNumberOfSeasons(Integer numberOfSeasons) {
+        this.numberOfSeasons = numberOfSeasons;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "gênero=" + genre +
+                ", título='" + title + '\'' +
+                ", número de temporadas=" + numberOfSeasons +
+                ", avaliação=" + rating +
+
+                ", atores='" + actors + '\'' +
+                ", url do poster='" + posterUrl + '\'' +
+                ", sinopse='" + plot + '\'';
+    }
 }
