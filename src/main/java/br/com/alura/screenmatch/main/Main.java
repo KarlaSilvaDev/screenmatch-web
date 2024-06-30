704e69b2019a07d91942ac41fecce7dd02e665be
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-06-30 17:54:47 -0300

    features: find series by title, find series by actor and search top 5 series

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -25,14 +25,17 @@ public class Main {
         this.repository = repository;
     }
 
-    public void showMenu(){
+    public void showMenu() {
         var option = -1;
-        while(option != 0) {
+        while (option != 0) {
             var menu = """
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar séries buscadas
-                                    
+                    4 - Buscar série por título
+                    5 - Buscar séries por ator
+                    6 - Top 5 séries
+                                                   
                     0 - Sair
                     """;
             System.out.println(menu);
@@ -49,6 +52,15 @@ public class Main {
                 case 3:
                     listSearchedSeries();
                     break;
+                case 4:
+                    findSeriesByTitle();
+                    break;
+                case 5:
+                    findSeriesByActor();
+                    break;
+                case 6:
+                    searchTop5Series();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -58,14 +70,14 @@ public class Main {
         }
     }
 
-    private void searchSerieWeb(){
+    private void searchSerieWeb() {
         SerieData data = getSerieData();
         Serie serie = new Serie(data);
         repository.save(serie);
         System.out.println(data);
     }
 
-    private SerieData getSerieData(){
+    private SerieData getSerieData() {
         System.out.println("Digite o nome da série para a busca:");
         var serieTitle = scanner.nextLine();
         var json = apiConsumer.getData(ADDRESS + serieTitle.replace(" ", "+") + API_KEY);
@@ -73,14 +85,12 @@ public class Main {
         return data;
     }
 
-    private void searchSerieEpisodes(){
+    private void searchSerieEpisodes() {
         listSearchedSeries();
         System.out.println("Escolha uma série pelo nomme: ");
         var serieTitle = scanner.nextLine();
 
-        Optional<Serie> serie = series.stream()
-                                        .filter(s -> s.getTitle().toLowerCase().contains(serieTitle.toLowerCase()))
-                                        .findFirst();
+        Optional<Serie> serie = repository.findByTitleContainingIgnoreCase(serieTitle);
 
         if (serie.isPresent()) {
             Serie serieFound = serie.get();
@@ -95,21 +105,51 @@ public class Main {
             seasons.forEach(System.out::println);
 
             List<Episode> episodes = seasons.stream()
-                                            .flatMap(s -> s.episodes().stream()
-                                                    .map(e -> new Episode(s.number(), e)))
-                                            .collect(Collectors.toList());
+                    .flatMap(s -> s.episodes().stream()
+                            .map(e -> new Episode(s.number(), e)))
+                    .collect(Collectors.toList());
 
             serieFound.setEpisodes(episodes);
             repository.save(serieFound);
-        }else{
+        } else {
             System.out.println("Série não encontrada.");
         }
     }
 
-    private void listSearchedSeries(){
+    private void listSearchedSeries() {
         this.series = repository.findAll();
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenre))
                 .forEach(System.out::println);
+    }
+
+    private void findSeriesByTitle() {
+        System.out.println("Escolha uma série pelo nome: ");
+        var serieTitle = scanner.nextLine();
+        Optional<Serie> searchedSeries = repository.findByTitleContainingIgnoreCase(serieTitle);
+
+        if (searchedSeries.isPresent()) {
+            System.out.println("Dados da série: " + searchedSeries.get());
+        } else {
+            System.out.println("Série não encontrada.");
+        }
+    }
+
+    private void findSeriesByActor(){
+        System.out.println("Digite o nome do ator ou atriz: ");
+        var actorName = scanner.nextLine();
+        System.out.println("Avaliações a partir de que valor?");
+        var rating = scanner.nextDouble();
+        List<Serie> series = repository.findByActorsContainingIgnoreCaseAndRatingGreaterThanEqual(actorName, rating);
+        System.out.println("Séries em que "+ actorName +" trabalhou: ");
+        series.forEach(s ->
+                System.out.println(s.getTitle() + " - avaliação: " + s.getRating())
+        );
+    }
+
+    private void searchTop5Series(){
+        List<Serie> top5Series = repository.findTop5ByOrderByRatingDesc();
+        top5Series.forEach(s ->
+                System.out.println(s.getTitle() + " - avaliação: " + s.getRating()));
     }
 }
--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -3,5 +3,13 @@ package br.com.alura.screenmatch.repository;
 import br.com.alura.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie> findByTitleContainingIgnoreCase(String serieTitle);
+
+    List<Serie> findByActorsContainingIgnoreCaseAndRatingGreaterThanEqual(String actorName, double rating);
+
+    List<Serie> findTop5ByOrderByRatingDesc();
 }
