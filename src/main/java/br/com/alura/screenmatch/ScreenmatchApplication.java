ce2450d3ce585d7c3bf232f5e990a04e5405e697
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-06-30 11:47:54 -0300

    created autowired repository attribute in ScreenmatchApplication class

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,16 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.main.Main;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Ctrl + Alt + O : tira todos os imports que não estão sendo usados na classe
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
+	@Autowired
+	private SerieRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
@@ -14,7 +18,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Main main = new Main();
+		Main main = new Main(repository);
 		main.showMenu();
 	}
 }
